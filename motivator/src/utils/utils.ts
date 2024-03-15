9899885b6d041ab5bd09daa911e4e2a41d7ea84c
0xMazout <lf@massa.net>
2024-03-15 18:00:11 +0100

    feat: add format address

--- motivator/src/utils/utils.ts
+++ motivator/src/utils/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatAddress(address: string) {
+  if (address.startsWith("0x") && address.length >= 20) {
+    const firstPart = address.slice(0, 6);
+    const lastPart = address.slice(-3);
+
+    return `${firstPart}...${lastPart}`;
+  }
+  return address;
 }
