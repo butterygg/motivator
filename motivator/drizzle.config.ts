03e49ec111fea31d4b1d02091122fd1c854fda2a
0xMazout <lucas.fabre.freelance@gmail.com>
2024-03-20 17:18:06 +0100

    feat: add drizzle config

---
+++ motivator/drizzle.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'drizzle-kit'
+export default {
+    schema: './src/app/api/db/schema.ts',
+    out: './drizzle',
+    driver: 'pg',
+    dbCredentials: {
+        connectionString:
+            'postgresql://motivatordb_owner:CMBw0bJj5uQl@ep-wispy-salad-a4yk2grq.us-east-1.aws.neon.tech/motivatordb?sslmode=require',
+    },
+} satisfies Config
