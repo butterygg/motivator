6b9f6b127fd237246febb23bff970092e39f1019
0xMazout <lucas.fabre.freelance@gmail.com>
2024-03-20 17:40:21 +0100

    feat:add db router feat: add

--- motivator/src/app/api/db/dbRouter.ts
+++ motivator/src/app/api/db/dbRouter.ts
@@ -1,8 +1,13 @@
 import { neon } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-http'
 
-const sql = neon(
+// contains the connection string to the neon database
+export const sql = neon(
     'postgresql://motivatordb_owner:CMBw0bJj5uQl@ep-wispy-salad-a4yk2grq.us-east-1.aws.neon.tech/motivatordb?sslmode=require'
 )
+
+// contains the drizzle object to send queries to the database
+export const db = drizzle(sql)
 
 // See https://neon.tech/docs/serverless/serverless-driver
 // for more information
