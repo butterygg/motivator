6f9f8f7417a68aa5e0732c1b700a6957ef233a86
0xMazout <lucas.fabre.freelance@gmail.com>
2024-03-22 18:40:57 +0100

    feat:add rewards

---
+++ motivator/src/app/api/rewards/add/route.ts
@@ -0,0 +1,35 @@
+// Send Rewards to specifics users based on their actions
+
+import { NextRequest } from 'next/server'
+import { db } from '@db/dbRouter'
+import { reward, user } from '../../../db/schema'
+
+/**
+ *
+ * @param request Will contain an Array of [{assessorAddress: string, userAddress: string, value: number}]
+ * @param response Send the status of the transaction
+ */
+export async function POST(request: NextRequest) {
+    const body = await request.json()
+
+    const userAddr = body.userAddress
+    const value = body.value
+
+    const rewardSent = await db.insert(reward).values({
+        amount: value,
+        user_address: userAddr,
+        date: new Date().toISOString(),
+    })
+
+    if (rewardSent) {
+        return Response.json({
+            status: 'ok',
+            message: `Reward of ${value} sent to ${userAddr}`,
+        })
+    } else {
+        return Response.json({
+            status: 'ko',
+            message: 'Error while sending the reward',
+        })
+    }
+}
