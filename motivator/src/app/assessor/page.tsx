32ca7c8b931014278036bb0c5eb87f67a5e68f7a
0xMazout <lucas.fabre.freelance@gmail.com>
2024-03-24 02:20:07 +0100

    feat: update assessor page to fit with DB

--- motivator/src/app/assessor/page.tsx
+++ motivator/src/app/assessor/page.tsx
@@ -1,42 +1,111 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import { DataTable } from '@/components/assessor/DataTable'
 import RewardedUsers from '@/components/assessor/RewardedUsers'
 import { User } from '@/types/data/user'
 import { Status } from '../../types/enum/status'
+import { useGetAssessorSlot } from '../../hooks/useGetAssessorSlot'
+import { useSession } from 'next-auth/react'
+import { useAccount } from 'wagmi'
+import { Address } from 'viem'
+import { AssessorSlot } from '../../types/data/assessorSlot'
 
 type Props = {}
 
 const HomeAssessor = (props: Props) => {
     // TODO : Mock of Data replace with API call using react query
+
+    const { address } = useAccount()
+
+    const { data, error, refetch } = useGetAssessorSlot({
+        assessorAddress: address ? address : '0x0',
+    })
+
+    const [assessorSlot, setAssessorSlot] = useState<AssessorSlot>()
+
+    useEffect(() => {
+        setAssessorSlot(data)
+    }, [data, error, refetch])
+
     const users: User[] = [
         {
             id: '1',
             addressName: '0x8753DE1914c4AB01F845b05b7BC146Bc898850A6',
-            volume: 100,
+            stat: {
+                volume: 500,
+                actions: 40,
+                user_address: '0x8753DE1914c4AB01F845b05b7BC146Bc898850A6',
+            },
             pnl: 100,
-            actions: 100,
             status: Status.Pending,
         },
         {
             id: '2',
             addressName: '0x8773DE1914c4AB01F845b05b7BC146Bc898850A6',
-            volume: 100,
+            stat: {
+                volume: 500,
+                actions: 40,
+                user_address: '0x8753DE1914c4AB01F845b05b7BC146Bc898850A6',
+            },
             pnl: 100,
-            actions: 100,
             status: Status.Rewarded,
         },
         {
             id: '3',
             addressName: '0xmazout.eth',
-            volume: 100,
+            stat: {
+                volume: 500,
+                actions: 40,
+                user_address: '0x8753DE1914c4AB01F845b05b7BC146Bc898850A6',
+            },
             pnl: 100,
-            actions: 100,
             status: Status.Pending,
         },
     ]
+
+    const dummyAssessorSlot: AssessorSlot = {
+        id: '1',
+        assessorID: '0x8753DE1914c4AB01F845b05b7BC146Bc898850A6',
+        rewards: [
+            {
+                date: '2021-09-10',
+                user_address: '0x8753DE1914c4AB01F845b05b7BC146Bc898850A6',
+                id: '1',
+                amount: 100,
+                assessor_slot_ID: '1',
+            },
+        ],
+        users: [
+            '0x8753DE1914c4AB01F845b05b7BC146Bc898850A6',
+            '0x8753DE1914c4AB01F845b05b7BC146Bc898850A6',
+            '0x8753DE1914c4AB01F845b05b7BC146Bc898850A6',
+        ],
+        done: false,
+        week: 0,
+        stats: [
+            {
+                user_address: '0x8753DE1914c4AB01F845b05b7BC146Bc898850A6',
+                actions: 40,
+                volume: 500,
+            },
+            {
+                user_address: '0x8753DE1914c4AB01F845b05b7BC146Bc898850A6',
+                actions: 40,
+                volume: 500,
+            },
+            {
+                user_address: '0x8753DE1914c4AB01F845b05b7BC146Bc898850A6',
+                actions: 40,
+                volume: 500,
+            },
+        ],
+    }
+
     return (
         <main className="flex flex-col lg:flex-row">
-            <DataTable />
+            <DataTable
+                assessorSlot={assessorSlot ? assessorSlot : dummyAssessorSlot}
+            />
             <RewardedUsers value={0} users={users} />
         </main>
     )
