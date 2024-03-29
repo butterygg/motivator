9314b06ab61ac74a9a24a1e22d9547b7514d8a4a
0xMazout <lucas.fabre.freelance@gmail.com>
2024-03-29 19:04:34 +0100

    fix:schema path

--- motivator/drizzle.config.ts
+++ motivator/drizzle.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'drizzle-kit'
 export const config = {
-    schema: './src/app/db/schema.ts',
+    schema: './src/server/db/schema.ts',
     out: './drizzle',
     driver: 'pg',
     dbCredentials: {
