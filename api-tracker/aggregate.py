d182a5e27e52ec8d7dc87811bbaf270967f47642
lajarre <alexandre.hajjar@gmail.com>
2024-04-18 01:22:01 +0530

    ✨ Aggregate api dump by contract, address, timestamp

---
+++ api-tracker/aggregate.py
@@ -0,0 +1,138 @@
+"""
+Hello
+"""
+
+import csv
+import json
+import pdb
+import sys
+import uuid
+from collections import defaultdict
+from dataclasses import dataclass, field
+from datetime import datetime, timedelta, timezone
+from decimal import Decimal, getcontext
+from enum import Enum, auto
+from typing import Dict, Iterator, List
+
+# Start block: 5663018, Apr-09-2024 06:56:48 PM +UTC.
+START_TIME = datetime(2024, 4, 9, 6, 0).replace(tzinfo=timezone.utc)
+# End: at noon Eastern on Thu 17th.
+END_TIME = datetime(2024, 4, 17, 16, 0).replace(tzinfo=timezone.utc)
+
+
+class EventType(Enum):
+    "Hello"
+    OpenLong = auto()
+    CloseLong = auto()
+    OpenShort = auto()
+    CloseShort = auto()
+    Initialize = auto()
+    AddLiquidity = auto()
+    RemoveLiquidity = auto()
+
+
+@dataclass
+class Event:
+    "Hello"
+    _type: EventType
+    block_timestamp: datetime
+    block_number: int
+    address: str
+    base_amount: int
+
+
+@dataclass
+class Row:
+    "Hello"
+    contract: str
+    timestamp: datetime
+    address: str
+    volume: int
+    action_count: int
+    _id: uuid.UUID = field(default_factory=uuid.uuid4)
+
+    def to_list(self):
+        return [
+            self._id,
+            self.contract,
+            self.timestamp,
+            self.address,
+            self.action_count,
+            self.volume,
+        ]
+
+    @staticmethod
+    def header():
+        return ["id", "contract", "timestamp", "address", "action_count", "volume"]
+
+
+def to_decimal(u256: str) -> Decimal:
+    getcontext().prec = 78  # U256 can have up to 78 decimal digits
+    u256_decimal = Decimal(u256)
+    return u256_decimal / Decimal("1000000000000000000")
+
+
+def to_event(data: Dict) -> Event:
+    return Event(
+        **{
+            **data,
+            "_type": EventType[data["_type"]],
+            "block_timestamp": to_datetime(data["block_timestamp"]),
+            "base_amount": to_decimal(data["base_amount"]),
+        }
+    )
+
+
+def to_datetime(iso8601: str) -> datetime:
+    return datetime.fromisoformat(iso8601.replace("Z", "+00:00"))
+
+
+def aggregate_period(
+    events: Dict[str, List[Event]], start_time: datetime, end_time: datetime
+) -> Iterator[Row]:
+    aggregates = defaultdict(lambda: {"volume": 0, "action_count": 0})
+
+    for cname, cevents in events.items():
+        for event in cevents:
+            if start_time < event.block_timestamp <= end_time:
+                key = (cname, event.block_timestamp, event.address)
+                aggregates[key]["volume"] += event.base_amount
+                aggregates[key]["action_count"] += 1
+
+    for (contract, timestamp, address), agg in aggregates.items():
+        yield Row(contract=contract, timestamp=timestamp, address=address, **agg)
+
+
+def aggregate(events: Dict[str, List[Event]]) -> Iterator[Row]:
+    start_time = START_TIME
+    end_time = start_time + timedelta(hours=1)
+    while end_time <= END_TIME:
+        yield from aggregate_period(events, start_time, end_time)
+
+        start_time = end_time
+        end_time += timedelta(hours=1)
+
+
+def main():
+    with open("./events.json", "r", encoding="utf-8") as f:
+        events_data = json.load(f)
+    events = {
+        cname: [to_event(e) for e in c_events_data]
+        for cname, c_events_data in events_data.items()
+    }
+
+    with open("./rows.csv", mode="w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(Row.header())
+        for row in aggregate(events):
+            writer.writerow(row.to_list())
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception:
+        # This captures the exception and starts a post-mortem debugging
+        info = sys.exc_info()
+        pdb.post_mortem(info[2])  # info[2] is the traceback object
+        raise
---
+++ api-tracker/dump_events.py
@@ -0,0 +1,531 @@
+"""
+Hello
+"""
+
+import json
+import os
+import pdb
+import sys
+from dataclasses import asdict, dataclass
+from enum import Enum, auto
+
+from moralis import evm_api
+from web3 import Web3
+
+START_BLOCK = 5663018
+END_BLOCK = 5718866
+
+MORALIS_API_KEY = os.environ["MORALIS_API_KEY"]
+
+CONTRACT = {
+    "4626": "0x392839da0dacac790bd825c81ce2c5e264d793a8",
+    "stETH": "0xff33bd6d7ed4119c99c310f3e5f0fa467796ee23",
+}
+
+
+class EventType(Enum):
+    "Hello"
+    OpenLong = auto()
+    CloseLong = auto()
+    OpenShort = auto()
+    CloseShort = auto()
+    Initialize = auto()
+    AddLiquidity = auto()
+    RemoveLiquidity = auto()
+
+
+@dataclass
+class EventStruct:
+    "Hello"
+    abi: dict
+    address_key: str
+
+    def topic(self, web3: Web3) -> str:
+        event_name = self.abi["name"]
+        input_types = ",".join([input["type"] for input in self.abi["inputs"]])
+        event_signature = f"{event_name}({input_types})"
+        return web3.keccak(text=event_signature).hex()
+
+
+EVENTS_STRUCT = {
+    EventType.OpenLong: EventStruct(
+        abi={
+            "anonymous": False,
+            "inputs": [
+                {
+                    "indexed": True,
+                    "internalType": "address",
+                    "name": "trader",
+                    "type": "address",
+                },
+                {
+                    "indexed": True,
+                    "internalType": "uint256",
+                    "name": "assetId",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "maturityTime",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "baseAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "vaultShareAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "bool",
+                    "name": "asBase",
+                    "type": "bool",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "bondAmount",
+                    "type": "uint256",
+                },
+            ],
+            "name": "OpenLong",
+            "type": "event",
+        },
+        address_key="trader",
+    ),
+    EventType.CloseLong: EventStruct(
+        abi={
+            "anonymous": False,
+            "inputs": [
+                {
+                    "indexed": True,
+                    "internalType": "address",
+                    "name": "trader",
+                    "type": "address",
+                },
+                {
+                    "indexed": True,
+                    "internalType": "address",
+                    "name": "destination",
+                    "type": "address",
+                },
+                {
+                    "indexed": True,
+                    "internalType": "uint256",
+                    "name": "assetId",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "maturityTime",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "baseAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "vaultShareAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "bool",
+                    "name": "asBase",
+                    "type": "bool",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "bondAmount",
+                    "type": "uint256",
+                },
+            ],
+            "name": "CloseLong",
+            "type": "event",
+        },
+        address_key="trader",
+    ),
+    EventType.OpenShort: EventStruct(
+        abi={
+            "anonymous": False,
+            "inputs": [
+                {
+                    "indexed": True,
+                    "internalType": "address",
+                    "name": "trader",
+                    "type": "address",
+                },
+                {
+                    "indexed": True,
+                    "internalType": "uint256",
+                    "name": "assetId",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "maturityTime",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "baseAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "vaultShareAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "bool",
+                    "name": "asBase",
+                    "type": "bool",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "baseProceeds",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "bondAmount",
+                    "type": "uint256",
+                },
+            ],
+            "name": "OpenShort",
+            "type": "event",
+        },
+        address_key="trader",
+    ),
+    EventType.CloseShort: EventStruct(
+        abi={
+            "anonymous": False,
+            "inputs": [
+                {
+                    "indexed": True,
+                    "internalType": "address",
+                    "name": "trader",
+                    "type": "address",
+                },
+                {
+                    "indexed": True,
+                    "internalType": "address",
+                    "name": "destination",
+                    "type": "address",
+                },
+                {
+                    "indexed": True,
+                    "internalType": "uint256",
+                    "name": "assetId",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "maturityTime",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "baseAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "vaultShareAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "bool",
+                    "name": "asBase",
+                    "type": "bool",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "basePayment",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "bondAmount",
+                    "type": "uint256",
+                },
+            ],
+            "name": "CloseShort",
+            "type": "event",
+        },
+        address_key="trader",
+    ),
+    EventType.Initialize: EventStruct(
+        abi={
+            "anonymous": False,
+            "inputs": [
+                {
+                    "indexed": True,
+                    "internalType": "address",
+                    "name": "provider",
+                    "type": "address",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "lpAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "baseAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "vaultShareAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "bool",
+                    "name": "asBase",
+                    "type": "bool",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "apr",
+                    "type": "uint256",
+                },
+            ],
+            "name": "Initialize",
+            "type": "event",
+        },
+        address_key="provider",
+    ),
+    EventType.AddLiquidity: EventStruct(
+        abi={
+            "anonymous": False,
+            "inputs": [
+                {
+                    "indexed": True,
+                    "name": "provider",
+                    "type": "address",
+                    "internal_type": "address",
+                },
+                {
+                    "indexed": False,
+                    "name": "lpAmount",
+                    "type": "uint256",
+                    "internal_type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "name": "baseAmount",
+                    "type": "uint256",
+                    "internal_type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "name": "vaultShareAmount",
+                    "type": "uint256",
+                    "internal_type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "name": "asBase",
+                    "type": "bool",
+                    "internal_type": "bool",
+                },
+                {
+                    "indexed": False,
+                    "name": "lpSharePrice",
+                    "type": "uint256",
+                    "internal_type": "uint256",
+                },
+            ],
+            "name": "AddLiquidity",
+            "type": "event",
+        },
+        address_key="provider",
+    ),
+    EventType.RemoveLiquidity: EventStruct(
+        abi={
+            "anonymous": False,
+            "inputs": [
+                {
+                    "indexed": True,
+                    "internalType": "address",
+                    "name": "provider",
+                    "type": "address",
+                },
+                {
+                    "indexed": True,
+                    "internalType": "address",
+                    "name": "destination",
+                    "type": "address",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "lpAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "baseAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "vaultShareAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "bool",
+                    "name": "asBase",
+                    "type": "bool",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "withdrawalShareAmount",
+                    "type": "uint256",
+                },
+                {
+                    "indexed": False,
+                    "internalType": "uint256",
+                    "name": "lpSharePrice",
+                    "type": "uint256",
+                },
+            ],
+            "name": "RemoveLiquidity",
+            "type": "event",
+        },
+        address_key="provider",
+    ),
+}
+
+
+@dataclass
+class EventData:
+    "Hello"
+    _type: EventType
+    block_timestamp: str
+    block_number: int
+    address: str
+    base_amount: int
+
+
+def result_and_cursor(
+    web3: Web3, contract_addr: str, event_type: EventType, event_struct, cursor=None
+):
+    params = {
+        "chain": "sepolia",
+        "order": "ASC",
+        "from_block": START_BLOCK,
+        "to_block": END_BLOCK,
+        "topic": event_struct.topic(web3),
+        "address": contract_addr,
+    }
+    if cursor:
+        params["cursor"] = cursor
+
+    result = evm_api.events.get_contract_events(
+        api_key=MORALIS_API_KEY,
+        body=event_struct.abi,
+        params=params,
+    )
+
+    return [
+        EventData(
+            _type=event_type,
+            block_timestamp=e["block_timestamp"],
+            block_number=e["block_number"],
+            address=e["data"][event_struct.address_key],
+            base_amount=e["data"]["baseAmount"],
+        )
+        for e in result["result"]
+    ], result["cursor"]
+
+
+class CustomEncoder(json.JSONEncoder):
+    "Hello"
+
+    def default(self, o):
+        if isinstance(o, EventType):
+            return o.name
+        return json.JSONEncoder.default(self, o)
+
+
+def events():
+    web3 = Web3()
+    es = {}
+
+    for contract_name, contract_addr in CONTRACT.items():
+        es[contract_name] = []
+
+        for event_type, event_struct in EVENTS_STRUCT.items():
+            new_events, cursor = result_and_cursor(
+                web3, contract_addr, event_type, event_struct
+            )
+            es[contract_name] += new_events
+
+            while cursor:
+                new_events, cursor = result_and_cursor(
+                    web3, contract_addr, event_type, event_struct, cursor
+                )
+                es[contract_name] += new_events
+
+    return es
+
+
+def main():
+    es = events()
+
+    with open("events.json", "w", encoding="utf-8") as f:
+        json.dump(
+            {
+                cname: [asdict(cevent) for cevent in cevents]
+                for cname, cevents in es.items()
+            },
+            f,
+            cls=CustomEncoder,
+            indent=4,
+        )
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception:
+        # This captures the exception and starts a post-mortem debugging
+        info = sys.exc_info()
+        pdb.post_mortem(info[2])  # info[2] is the traceback object
+        raise
