8e37f278e72958cf29427092e73d381a581eb3a4
0xMazout <lucas.fabre.freelance@gmail.com>
2024-04-22 12:33:13 +0200

    feat: add Architecture

--- motivator/src/app/assessor/slot/[id]/page.tsx
+++ motivator/src/app/assessor/slot/[id]/page.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 type Props = {}
 
-import DataTableContainer from '@/components/container/DataTableContainer'
+import { DataTableContainerLeaderboardAssessor } from '@/components/container/DataTableContainerAssessor'
 
 const HomeAssessor = (props: Props) => {
     return (
         <main className="flex lg:flex-row w-full">
-            <DataTableContainer />
+            <DataTableContainerLeaderboardAssessor />
         </main>
     )
 }
