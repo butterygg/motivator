c2973dda6462a66bea2d6f2fb0f31dd3682d8268
0xMazout <lucas.fabre.freelance@gmail.com>
2024-04-25 11:16:48 +0200

    ✨ Update Navbar

--- motivator/src/components/globals/NavBar.tsx
+++ motivator/src/components/globals/NavBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { ToggleTheme } from './ToggleTheme'
+import Link from 'next/link'
 
 export const YourApp = () => {
     return
@@ -9,10 +10,16 @@ type Props = {}
 
 const NavBar = (props: Props) => {
     return (
-        <div className="max-md:flex-col w-full p-8 justify-between flex">
-            <div>
-                <h1 className="font-bold text-2xl">Motivator</h1>
-                <h2 className="font-semibold text-xl">A Buttery Good Game</h2>
+        <div className="items-center max-md:flex-col w-full p-8 justify-between flex">
+            <div className="flex items-center gap-4">
+                <Link href={'/'}>
+                    <h1 className="font-bold text-2xl">Motivator</h1>
+                    <h2 className="font-semibold text-xl">
+                        A Buttery Good Game
+                    </h2>
+                </Link>
+                <Link href={'/payment'}>Purchase Slot</Link>
+                <Link href={'/audit'}>Audit</Link>
             </div>
             <div className="flex items-center gap-2">
                 <ToggleTheme />
