da4632179855f86dd98d36c3e200d52aa244c99e
0xMazout <lucas.fabre.freelance@gmail.com>
2024-04-25 16:12:45 +0200

    ✨ servers actions

--- motivator/src/server/actions/globals/getAllAssessorSlotsAudit.ts
+++ motivator/src/server/actions/globals/getAllAssessorSlotsAudit.ts
@@ -56,7 +56,6 @@ export async function getAllAssessorSlotsAudit() {
             }
             return assessor
         })
-        console.log('result', result)
         return result
     }
     const assessorsSlotsFilled = await buildAssessorSlot()
