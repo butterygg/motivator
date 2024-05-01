bb8162be487ca76cbcbe2e27a40363c63378a8ae
lajarre <alexandre.hajjar@gmail.com>
2024-05-01 10:39:30 +0100

    Revert "✨ Create udpatedb script"
    This reverts commit 399b2f3f8a467562e04fc6e83a3acc1b73fcceca.

--- api-tracker/aggregate.py
+++ api-tracker/aggregate.py
@@ -1,13 +1,14 @@
 """
-Aggregate
+Hello
 """
 
 import csv
 import json
-import os
 import pdb
 import sys
 import uuid
+import os
+
 from collections import defaultdict
 from dataclasses import dataclass, field
 from datetime import date, datetime, time, timedelta, timezone
@@ -15,12 +16,12 @@ from decimal import Decimal, getcontext
 from enum import Enum, auto
 from typing import Dict, Iterator, List
 
-START_DATE = datetime.strptime(os.environ["START_DATE"], "%Y-%m-%d").date()
-END_DATE = datetime.strptime(os.environ["END_DATE"], "%Y-%m-%d").date()
+START_DATE = datetime.strptime(os.environ['START_DATE'], '%Y-%m-%d').date()
+END_DATE = datetime.strptime(os.environ['END_DATE'], '%Y-%m-%d').date()
 
 
 class EventType(Enum):
-    "Event Type"
+    "Hello"
     OpenLong = auto()
     CloseLong = auto()
     OpenShort = auto()
