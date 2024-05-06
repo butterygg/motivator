6aad3c2fc7d57d7692a7145b0c2a2d7b398b6763
0xMazout <lucas.fabre.freelance@gmail.com>
2024-05-06 12:52:41 +0200

    ✨ Add Computation Grade

--- motivator/src/utils/utils.ts
+++ motivator/src/utils/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { Grade } from '../types/enum/grade'
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
@@ -23,4 +24,17 @@ export function transformNumberK(value: number) {
         return `${(value / 1000).toFixed(1)}k`
     }
     return value.toFixed(2)
+}
+
+export function auditComputation(audit: Grade, rewards?: number) {
+    switch (audit) {
+        case Grade.A:
+            return (rewards ? rewards : 0) + 20
+        case Grade.B:
+            return (rewards ? rewards : 0) + 2
+        case Grade.C:
+            return rewards ? rewards : 0
+        default:
+            return rewards ? rewards : 0
+    }
 }
