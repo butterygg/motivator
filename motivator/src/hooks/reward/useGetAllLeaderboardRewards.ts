4948e864e62da4b46ce84c95e22fc51a83864330
0xMazout <lucas.fabre.freelance@gmail.com>
2024-05-06 12:52:30 +0200

    ✨ Add hooks to get AllRewards for leaderboard

---
+++ motivator/src/hooks/reward/useGetAllLeaderboardRewards.ts
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+import { getAllLeaderboardRewards } from '@/server/actions/reward/getAllLeaderboardRewards'
+type Props = {
+    userAddr: string
+}
+
+/**
+ * This hook is used to fetch the assessor slot of the assessor
+ * @param {Props} props
+ *
+ *
+ */
+const useGetAllLeaderboardRewards = () => {
+    const { data, refetch, status, error } = useQuery({
+        queryKey: ['getRewardUsers'],
+        queryFn: async () => {
+            return getAllLeaderboardRewards()
+        },
+        refetchInterval: 1000 * 3,
+    })
+    return { data, refetch, error, status }
+}
+
+export { useGetAllLeaderboardRewards }
---
+++ motivator/src/server/actions/reward/getAllLeaderboardRewards.ts
@@ -0,0 +1,85 @@
+'use server'
+import { and, eq, ne } from 'drizzle-orm'
+import { db } from '@db/dbRouter'
+import { audit, reward, assessor_slot, user, assessor } from '@db/schema'
+import { auditComputation } from '../../../utils/utils'
+import { Grade } from '../../../types/enum/grade'
+import { LeaderboardDatatable } from '../../../components/leaderboard/DataTableLeaderboard'
+
+export const getAllLeaderboardRewards = async () => {
+    const rewards = await db
+        .select()
+        .from(reward)
+        .where(ne(reward.amount, 0))
+        .execute()
+
+    const users = await db.select().from(user).execute()
+    const assessors = await db.select().from(assessor).execute()
+    if (!rewards) {
+        return {
+            status: 'ko',
+            message: 'No rewards find',
+        }
+    }
+
+    const audits = await db.select().from(audit).execute()
+
+    // Build the rewards for each user parsing his rewards and potentially his audit
+    const buildRewards = () => {
+        const res: LeaderboardDatatable[] = []
+        users.forEach(async (element) => {
+            // Find the rewards for this user
+            const rewardsForUser = rewards.filter(
+                (reward) => reward.user_address === element.address
+            )
+            // Compute rewards for this user
+            let totalRewards = 0
+            rewardsForUser.forEach((element) => {
+                totalRewards += element.amount as number
+            })
+
+            const assessor = assessors.find(
+                (assessor) => assessor.address === element.address
+            )
+
+            // init global variables
+            let totalAudit = 0
+            let isTestnetMember = false
+            // Test if the user is an Assessor or not
+            if (assessor) {
+                isTestnetMember = true
+                // Then find his slots and if his slots has been audited and
+                const assessorSlots = await db
+                    .select()
+                    .from(assessor_slot)
+                    .where(eq(assessor_slot.assessor_ID, element.address))
+                    .execute()
+                // add the rewards converted from grade audit to the total counter for this user
+                assessorSlots.forEach((assessor) => {
+                    assessor.assessor_ID
+                    const audit = audits.find(
+                        (audit) => audit.assessor_slot_id === assessor.id
+                    )
+                    totalAudit += auditComputation(audit?.audit_grade as Grade)
+                })
+            }
+            res.push({
+                id: element.address,
+                addressName: element.address,
+                rewardsReceived: {
+                    rewards: totalRewards,
+                    audit: totalAudit,
+                },
+                total: totalRewards + totalAudit,
+                isTestnetMember: isTestnetMember,
+            })
+        })
+        return res
+    }
+
+    return {
+        res: buildRewards(),
+        status: 'ok',
+        message: 'Rewards Find',
+    }
+}
