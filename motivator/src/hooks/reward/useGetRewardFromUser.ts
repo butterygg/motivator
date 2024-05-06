877376507a40e3347c7414b4681acf7155503d25
0xMazout <lucas.fabre.freelance@gmail.com>
2024-05-06 10:59:03 +0200

    ✨ Add Hooks and Server Actions to get Reward for specific user

---
+++ motivator/src/hooks/reward/useGetRewardFromUser.ts
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+import { getRewardFromUser } from '@/server/actions/reward/getRewardFromUser'
+type Props = {
+    userAddr: string
+}
+
+/**
+ * This hook is used to fetch the assessor slot of the assessor
+ * @param {Props} props
+ *
+ *
+ */
+const useGetRewardFromUser = ({ userAddr }: Props) => {
+    const { data, refetch, status, error } = useQuery({
+        queryKey: ['getRewardUsers'],
+        queryFn: async () => {
+            return getRewardFromUser(userAddr)
+        },
+        refetchInterval: 1000 * 3,
+    })
+    return { data, refetch, error, status }
+}
+
+export { useGetRewardFromUser }
