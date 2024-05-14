90b0a135c294ca0e18feed3a6e8c809d5441e6d2
0xMazout <lucas.fabre.freelance@gmail.com>
2024-05-14 16:16:36 +0200

    💄 dark to gray 💄 Update Dates

--- motivator/src/components/statistics/LineChart.tsx
+++ motivator/src/components/statistics/LineChart.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AreaChart } from '@tremor/react'
 import { DataSetChartTrading } from '@/components/assessor/DialogUserData'
+import { cn } from '../../utils/utils'
 
 // const valueFormatter = function (number: number) {
 //     return (
@@ -17,23 +18,93 @@ export type Props = {
 }
 
 export function LineChart({ title, dataset, type }: Props) {
-    dataset.sort((a, b) => {
-        return (
-            new Date(a.date as string).getTime() -
-            new Date(b?.date as string).getTime()
-        )
-    })
-    const valueFormatter = function (number: number) {
-        if (type === 'ETH')
+    dataset
+        .sort((a, b) => {
             return (
-                new Intl.NumberFormat('us', { maximumSignificantDigits: 2 })
-                    .format(number)
-                    .toString() + ' ETH'
+                new Date(a.date as string).getTime() -
+                new Date(b?.date as string).getTime()
             )
+        })
+        .map((data) => {
+            data.date = new Date(data.date as string).toLocaleDateString()
+            return data
+        })
+    const valueFormatter = function (number: number) {
         return (
             new Intl.NumberFormat('us', { maximumSignificantDigits: 2 })
                 .format(number)
-                .toString() + ' DAI'
+                .toString() + ` ${type}`
+        )
+    }
+
+    const customTooltip = (props: {
+        active: any
+        payload: any
+        label: any
+    }) => {
+        const { active, payload, label } = props
+        return (
+            <div className="w-fit rounded-tremor-default border border-tremor-border bg-tremor-background p-2 text-tremor-default shadow-tremor-dropdown">
+                <p className="items-center w-full justify-center text-tremor-content-strong">
+                    {label}
+                </p>
+                {payload.map(
+                    (
+                        category: {
+                            color: any
+                            dataKey:
+                                | string
+                                | number
+                                | boolean
+                                | React.ReactElement<
+                                      any,
+                                      string | React.JSXElementConstructor<any>
+                                  >
+                                | Iterable<React.ReactNode>
+                                | React.ReactPortal
+                                | Promise<React.AwaitedReactNode>
+                                | null
+                                | undefined
+                            value:
+                                | string
+                                | number
+                                | boolean
+                                | React.ReactElement<
+                                      any,
+                                      string | React.JSXElementConstructor<any>
+                                  >
+                                | Iterable<React.ReactNode>
+                                | React.ReactPortal
+                                | Promise<React.AwaitedReactNode>
+                                | null
+                                | undefined
+                        },
+                        idx: React.Key | null | undefined
+                    ) => (
+                        <div key={idx} className="flex flex-1 space-x-2.5">
+                            <div
+                                className={cn(
+                                    `flex w-1 flex-col rounded`,
+                                    `bg-${category.color}-500`
+                                )}
+                            />
+                            <div className="flex gap-2 items-center">
+                                <p className="text-tremor-content">
+                                    {category.dataKey}
+                                </p>
+                                <p className="font-medium text-tremor-content-emphasis">
+                                    {new Intl.NumberFormat('us', {
+                                        maximumSignificantDigits: 2,
+                                    })
+                                        .format(category.value as number)
+                                        .toString()}{' '}
+                                    {` ${type}`}
+                                </p>
+                            </div>
+                        </div>
+                    )
+                )}
+            </div>
         )
     }
 
@@ -100,7 +171,7 @@ export function LineChart({ title, dataset, type }: Props) {
         },
     ]
     return (
-        <div className="border rounded-lg p-3">
+        <div className="border dark:bg-tremor-background-emphasis rounded-lg p-3">
             <h3 className="text-tremor-title text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">
                 {title}
             </h3>
@@ -108,13 +179,14 @@ export function LineChart({ title, dataset, type }: Props) {
                 ${value}
             </p> */}
             <AreaChart
-                className="h-80 text-white"
+                className="h-80 dark:text-dark-tremor-content-strong"
                 data={dataset}
                 index="date"
                 showXAxis={false}
                 categories={['Short', 'Long']}
                 colors={['red', 'blue']}
                 valueFormatter={valueFormatter}
+                customTooltip={customTooltip}
                 yAxisWidth={70}
             />
         </div>
