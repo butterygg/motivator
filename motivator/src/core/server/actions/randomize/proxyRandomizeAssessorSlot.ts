a90a616f15c76e927a2045f057203bf8dab68dd4
0xMazout <lucas.fabre.freelance@gmail.com>
2024-06-18 18:10:06 +0200

    ⚡ Comments

--- motivator/src/core/server/actions/randomize/proxyRandomizeAssessorSlot.ts
+++ motivator/src/core/server/actions/randomize/proxyRandomizeAssessorSlot.ts
@@ -3,6 +3,13 @@ import { getScoreAndAddr } from '@protocols/hyperdrive/server/actions/randomize/
 type Props = {
     protocol: string
 }
+/**
+ * This proxy is use to load the function associated with the specified protocol
+ * |ImplementProtocol| When you implement a new protocol
+ * just add a new case in the switch statement with the name of the protocol and the new function to acquire Score and Address to Randomize
+ * @param param0
+ * @returns
+ */
 export const proxyRandomizeAssessorSlot = ({ protocol }: Props) => {
     switch (protocol) {
         case 'hyperdrive':
