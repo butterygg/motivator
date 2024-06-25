11f594c27703895d0cc2f3097248c60e1687c4ca
0xMazout <lucas.fabre.freelance@gmail.com>
2024-06-25 16:55:13 +0200

    💄 format

--- motivator/src/app/payment/page.tsx
+++ motivator/src/app/payment/page.tsx
@@ -5,7 +5,6 @@ import React from 'react'
 import PaymentContainer from '@/components/container/PaymentContainer'
 
 const Payment = () => {
-
     return (
         <section className="mx-auto w-fit p-12">{<PaymentContainer />}</section>
     )
